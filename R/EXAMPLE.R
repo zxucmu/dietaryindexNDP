ae3c083948c19f8ca73c2befc4760244a41026c4
zxucmu <zxu@cmu.edu.cn>
2024-10-24 06:17:06 +0800

    Delete R directory

--- R/EXAMPLE.R
+++
@@ -1,6 +0,0 @@
-#' @export
-EXAMPLE <- function(FILE = NULL){
-  file <- FILE
-  load(file = paste0(system.file(package = 'dietaryindexNDP'), '/data/', file, ".RData"))
-  return(get(paste0(file)))
-}
